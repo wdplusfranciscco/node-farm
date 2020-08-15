43e57751ecc663a7d8e762b43f755d0f355fd61d
wdplusfranciscco <wdplusfranciscco@gmail.com>
2020-08-15 08:53:24 -0500

    Add modules folder to convert the  replaceTemplate function into a module. This is ready to become a release candidate for version 1.1.0.

--- index.js
+++ index.js
@@ -2,24 +2,12 @@
 // Version 1.0.0
 
 
-const fs = require('fs');
-const http = require('http');
-const url = require('url');
+const fs              = require('fs');
+const http            = require('http');
+const url             = require('url');
+const replaceTemplate = require('./modules/replaceTemplate.js');
 
 
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutirents);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-    
-    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    return output;
-} 
 // Retrieve data during start and buffer it in for further retrieval.
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
